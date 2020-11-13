213972a2d409102d2194b889631f5e943e5e909e
yudi iriyanto <52985439+riyan-web@users.noreply.github.com>
2020-11-13 13:11:54 +0700

    halaman artikel dinamis terhubung dengan DB

--- application/views/profile_pmii/artikel.php
+++ application/views/profile_pmii/artikel.php
@@ -5,7 +5,7 @@ $query_artikel = "SELECT *
                 JOIN `tb_user` ON  `tb_konten`.`pembuat` = `tb_user`.`id`
                 WHERE`tb_konten`.`status` = 2
                 ";
-$artikel = $this->db->query($query_artikel)->row_array();
+$artikel = $this->db->query($query_artikel)->result();
 ?>
 
 <main id="main">
@@ -29,87 +29,22 @@ $artikel = $this->db->query($query_artikel)->row_array();
         <div class="container">
 
             <div class="row">
-
-                <div class="col-lg-6" data-aos="fade-up">
-                    <div class="testimonial-item">
-                        <img src="<?php echo base_url() ?>assets/frontend/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                        <h3><?php echo $artikel['judul']; ?></h3>
-                        <h4>Dibuat : <?php echo $artikel['tgl_buat']; ?> &amp; Oleh : <?php echo $artikel['username']; ?></h4>
-                        <p>
-                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                            <?php echo substr($artikel['isi_konten'], 0, 150) . "..."; ?>
-                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                        </p>
+                <?php foreach ($artikel as $art) { ?>
+                    <div class="col-lg-6" data-aos="fade-up">
+                        <div class="testimonial-item">
+                            <img src="<?php echo base_url('assets/frontend/img/img_artikel/') . $art->foto_artikel; ?>" class="testimonial-img" alt="">
+                            <h3><?php echo $art->judul ?></h3>
+                            <h4>Dibuat : <?php echo $art->tgl_buat ?> &amp; Oleh : <?php echo $art->username ?></h4>
+                            <p>
+                                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                                <?php echo substr($art->isi_konten, 0, 150) . "..."; ?>
+                                <a href="<?php echo base_url('beranda/view_artikel') ?>" class="btn-buy">Lanjut Baca</a>
+                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                            </p>
+                        </div>
                     </div>
-                </div>
-
-                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="testimonial-item mt-4 mt-lg-0">
-                        <img src="<?php echo base_url() ?>assets/frontend/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                        <h3>Sara Wilsson</h3>
-                        <h4>Designer</h4>
-                        <p>
-                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-                    <div class="testimonial-item mt-4">
-                        <img src="<?php echo base_url() ?>assets/frontend/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                        <h3>Jena Karlis</h3>
-                        <h4>Store Owner</h4>
-                        <p>
-                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
-                    <div class="testimonial-item mt-4">
-                        <img src="<?php echo base_url() ?>assets/frontend/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                        <h3>Matt Brandon</h3>
-                        <h4>Freelancer</h4>
-                        <p>
-                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="400">
-                    <div class="testimonial-item mt-4">
-                        <img src="<?php echo base_url() ?>assets/frontend/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                        <h3>John Larson</h3>
-                        <h4>Entrepreneur</h4>
-                        <p>
-                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="500">
-                    <div class="testimonial-item mt-4">
-                        <img src="<?php echo base_url() ?>assets/frontend/img/testimonials/testimonials-6.jpg" class="testimonial-img" alt="">
-                        <h3>Emily Harison</h3>
-                        <h4>Store Owner</h4>
-                        <p>
-                            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Eius ipsam praesentium dolor quaerat inventore rerum odio. Quos laudantium adipisci eius. Accusamus qui iste cupiditate sed temporibus est aspernatur. Sequi officiis ea et quia quidem.
-                            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                        </p>
-                    </div>
-                </div>
-
+                <?php } ?>
             </div>
-
         </div>
     </section><!-- End Testimonials Section -->
 
