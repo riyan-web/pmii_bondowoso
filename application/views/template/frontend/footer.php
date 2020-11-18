900acec14e50a49dd05dbbb9c88d71af18cde933
AnaFarida <anafarida1820@gmail.com>
2020-11-18 16:13:51 +0700

    Update footer

--- application/views/template/frontend/footer.php
+++ application/views/template/frontend/footer.php
@@ -6,7 +6,7 @@
     <div class="row">
 
       <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>Flattern</h3>
+        <h3>Alamat</h3>
         <p>
           A108 Adam Street <br>
           New York, NY 535022<br>
@@ -19,32 +19,14 @@
       <div class="col-lg-2 col-md-6 footer-links">
         <h4>Useful Links</h4>
         <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+        <li class=""><a href="<?= base_url('beranda') ?>">Home</a></li>
+          <li class=""><a href="<?= base_url('beranda/about') ?>">Tentang Kami</a></li>
+          <li><a href="<?= base_url('beranda/artikel') ?>">Artikel</a></li>
+          <li><a href="services.html">Program Kerja</a></li>
+          <li><a href="<?= base_url('beranda/berita') ?>">Berita</a></li>
         </ul>
       </div>
 
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Services</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-4 col-md-6 footer-newsletter">
-        <h4>Join Our Newsletter</h4>
-        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-        <form action="" method="post">
-          <input type="email" name="email"><input type="submit" value="Subscribe">
-        </form>
-      </div>
 
     </div>
   </div>
