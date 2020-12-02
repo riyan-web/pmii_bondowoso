3ee41cac0c10b841d86a885076559117d0f36003
Ilham Robby Sanjaya <ilhamrobbysanjaya220@gmail.com>
2020-12-02 08:16:39 +0700

    kl

--- application/models/M_struktur.php
+++ application/models/M_struktur.php
@@ -6,7 +6,7 @@ class M_struktur extends CI_Model
     function getStrukturCabang()
     {
 
-        return $this->db->query("SELECT `tb_strukturkom`.`tipe`, `tb_kader`.`nama_kader`, `tb_kader`.`foto_kader` FROM `tb_strukturkom` 
+        return $this->db->query("SELECT `tb_strukturkom`.`tipe`, `tb_kader`.`nama`, `tb_kader`.`foto` FROM `tb_strukturkom` 
         JOIN `tb_kader` ON `tb_strukturkom`.`kader_id` = `tb_kader`.`id` 
         JOIN `tb_komisariat` ON `tb_strukturkom`.`komisariat_id` = `tb_komisariat`.`id` 
         WHERE `tb_strukturkom`.`komisariat_id` = 1 ");
